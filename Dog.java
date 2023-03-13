944c4b4571ee69eb93a95e5c66054182f1a5d3e9
mmlado-cpp <98682021+mmlado-cpp@users.noreply.github.com>
2023-03-12 22:13:44 -0700

    implemented paw mutliplicity
    initialized paw list to size 4, since dogs must be created with 4 paws

--- Dog.java
+++ Dog.java
@@ -1,12 +1,17 @@
 package HW2;
+import java.util.ArrayList;
 //5d
 import java.util.List;
 
 public class Dog {
     private String breed;
     private String name;
-    private List<Paw> paws;
+    private List<Paw> paws = new ArrayList<>(4);
     public Dog() {
+        for (int i = 0; i < 4; i++) {
+            this.paws.add(null); //dog must have 4 paws
+        }
+        
     }
 }
 
